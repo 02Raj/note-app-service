c1234b12ff1b92f95522bc505b1359a08f57ec14
Divyansh Raj <divyanshraj02@gmail.com>
2025-11-18 00:44:12 +0530

    remove restore service

--- server/routes/notes.routes.js
+++ server/routes/notes.routes.js
@@ -8,19 +8,14 @@ const {
   getByTopic,
   getBySubtopic,
   remove,
-  update,
-  restore,           // 👈 IMPORT RESTORE
+  update
 } = require("../controllers/notes.controller");
 
-// routes
 router.post("/", authenticate, create);
-router.put("/:id", authenticate, update);
+router.put("/:id", authenticate, update); 
 router.get("/", authenticate, getAll);
 router.get("/topic/:topicId", authenticate, getByTopic);
 router.get("/subtopic/:subtopicId", authenticate, getBySubtopic);
 router.delete("/:id", authenticate, remove);
-
-// ⚠️ NEW ROUTE (Restore Deleted Note)
-router.post("/restore/:id", authenticate, restore);
 
 module.exports = router;
